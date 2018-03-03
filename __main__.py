b28991cfbf88b7b69eb1e0deeb6d806bb5ee4d26
Jackkell <bronzeeagle1@hotmail.com>
2018-03-03 13:17:22 -0800

    Add comments and refactor names

--- __main__.py
+++ __main__.py
@@ -1,43 +1,41 @@
-from typing import List, Dict
-import pandas
-import pprint
 from pandas import DataFrame
-import numpy
 
 
-def createViterbiMatrix(inputSentence: str,
-                        wordPosProbabilityMap,
-                        posMarchovChainMap) -> DataFrame:
-    columns: List[str] = inputSentence.split()
-    columns.insert(0, "<start>")
-    columns.append("<stop>")
-    rows: List[str] = list(posMarchovChainMap.keys())
+def createViterbiMatrix(inputSentence, wordPosProbabilityMap, posMarchovChainMap):
+    """Creates the Viterbi decoding matrix for a given input sentence"""
+    # Tokenize input sentence
+    tokens = inputSentence.split()
+    tokens.insert(0, "<start>")
+    tokens.append("<stop>")
 
-    viterbiMatrix = [[(0, None) for x in range(len(rows))] for y in range(len(columns))]
-    pp = pprint.PrettyPrinter(depth=6, width=300)
+    # Get list of all possible POS tags
+    poss = list(posMarchovChainMap.keys())
+
+    # Initialize viterbi matrix
+    viterbiMatrix = [[(0, None) for x in range(len(tokens))] for y in range(len(poss))]
     viterbiMatrix[0][0] = 1, None
-    # pp.pprint(viterbiMatrix)
 
-    for columnIndex in range(1, len(columns)):
-        token = columns[columnIndex]
-        previousColumnIndex = columnIndex - 1
-        for rowIndexFrom, posFrom in enumerate(rows):
-            previousProbability, previousPoint = viterbiMatrix[rowIndexFrom][previousColumnIndex]
-            for rowIndexTo, posTo in enumerate(rows):
+    for tokenIndex in range(1, len(tokens)):
+        token = tokens[tokenIndex]
+        previousColumnIndex = tokenIndex - 1
+        for posIndexFrom, posFrom in enumerate(poss):
+            previousProbability, previousPointer = viterbiMatrix[posIndexFrom][previousColumnIndex]
+            for posIndexTo, posTo in enumerate(poss):
                 wordPosProbability = wordPosProbabilityMap.get(token).get(posTo, 0)
-                stateChangeProbability = posMarchovChainMap.get(posFrom).get(posTo, 0)
-                newPointer = (rowIndexFrom, previousColumnIndex)
-                newScore = previousProbability * wordPosProbability * stateChangeProbability
-                currentScore, currentPoint = viterbiMatrix[rowIndexTo][columnIndex]
-                if currentScore < newScore:
-                    viterbiMatrix[rowIndexTo][columnIndex] = newScore, newPointer
-    # pp.pprint(viterbiMatrix)
-    viterbiDF = DataFrame(viterbiMatrix, index=rows, columns=columns)
+                posTransitionProbability = posMarchovChainMap.get(posFrom).get(posTo, 0)
+                newPointer = (posIndexFrom, previousColumnIndex)
+                newProbability = previousProbability * wordPosProbability * posTransitionProbability
+                currentProbability, currentPoint = viterbiMatrix[posIndexTo][tokenIndex]
+                if currentProbability < newProbability:
+                    viterbiMatrix[posIndexTo][tokenIndex] = newProbability, newPointer
+
+    viterbiDF = DataFrame(viterbiMatrix, index=poss, columns=tokens)
     return viterbiDF
 
 
 def backtraceViterbiDF(vertbiDF):
-    prediction = ["<end>"]
+    """Return the POS list found in the given vertibi matrix"""
+    prediction = ["<stop>"]
     probability, pointer = vertbiDF["<stop>"]["<stop>"]
     while pointer:
         pos = vertbiDF.index[pointer[0]]
@@ -47,6 +45,7 @@ def backtraceViterbiDF(vertbiDF):
 
 
 def main():
+    # In class test case example to prove that the implementation works
     posMarchovChainMap = {
         "<start>": {
             "noun": 0.8,
@@ -65,7 +64,6 @@ def main():
         "<stop>": {
         }
     }
-
     wordPosProbabilityMap = {
         "fish": {
             "verb": 0.5,
@@ -82,14 +80,15 @@ def main():
             "<stop>": 1
         }
     }
-
     inputSentence = "fish sleep"
 
     viterbiDF = createViterbiMatrix(inputSentence, wordPosProbabilityMap, posMarchovChainMap)
-    print(inputSentence)
-    print(viterbiDF)
-    print(backtraceViterbiDF(viterbiDF))
-
+    print("------In Class Test Case------")
+    print("Input Sentence:", inputSentence)
+    print("Viterbi Matrix:\n", viterbiDF)
+    print("POS Output:", backtraceViterbiDF(viterbiDF))
+    print()
+    # The answer to the problem homework problem
     posMarchovChainMap = {
         "<start>": {
             "noun": 0.2,
@@ -133,9 +132,11 @@ def main():
     inputSentence = "learning changes throughly"
 
     viterbiDF = createViterbiMatrix(inputSentence, wordPosProbabilityMap, posMarchovChainMap)
-    print(inputSentence)
-    print(viterbiDF)
-    print(backtraceViterbiDF(viterbiDF))
+    print("------Homework Answer------")
+    print("Input Sentence:", inputSentence)
+    print("Viterbi Matrix:\n", viterbiDF)
+    print("POS Output:", backtraceViterbiDF(viterbiDF))
+    print()
 
 
 if __name__ == '__main__':
